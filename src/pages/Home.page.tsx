a6f5b77a4db859735c004f385968e13915293044
nsaraiva <nuno.saraiva@wacestudio.com>
2024-03-20 12:07:01 +0000

    dev

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,8 +1,9 @@
-import { Button, Center, Group, Modal, SimpleGrid, Stack, Text, TextInput, Title } from "@mantine/core";
+import { AppShell, Button, Center, Flex, Grid, Group, Modal, SimpleGrid, Space, Stack, Text, TextInput, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { Alchemy, Network } from "alchemy-sdk";
 import { useState } from "react";
 import { Block } from "../models/Block";
+import ColorThemeSwitcher from "../components/ColorThemeSwitcher/ColorThemeSwitcher";
 
 export function HomePage() {
   const [text, setText] = useState('');
@@ -25,29 +26,46 @@ export function HomePage() {
   }
 
   return (
-    <>
-      <Modal opened={opened} onClose={close} title={block?.number} size={"xl"}>
-        <SimpleGrid cols={2}>
-          <Text size="xs">Hash</Text>
-          <Text size="xs">{block?.hash}</Text>
-        </SimpleGrid>
-      </Modal>
-      <Center>
-        <Stack>
-          <Title>show me the block</Title>
+    <AppShell
+      header={{ height: 60 }}
+      footer={{ height: 60 }}
+      padding="md"
+    >
+      <AppShell.Header withBorder={false}>
+        <Group justify="flex-end" h="100%" px="md">
+          <ColorThemeSwitcher />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Main>
+        <Modal opened={opened} onClose={close} title={`Block number ${block?.number}`} size={"xl"}>
+          <Grid>
+            <Grid.Col span={2}>
+              <Text size="xs">Hash</Text>
+            </Grid.Col>
+            <Grid.Col span={10}>
+              <Text size="xs">{block?.hash}</Text>
+            </Grid.Col>
+          </Grid>
+        </Modal>
+        <Center>
+          <Stack gap={"xl"}>
+            <Title ff={"mono"} lts={10}>show me the block</Title>
 
-          <Group>
-            <TextInput value={text} onChange={e => setText(e.target.value)} />
+            <Group>
+              <TextInput variant="unstyled" placeholder="Enter a numer" autoFocus size="xl" value={text} onChange={e => setText(e.target.value)} />
 
-            <Button onClick={openModal}>
-              Go
-            </Button>
-          </Group>
-          <Center>
-            <Text size="sm">2024 nfpsaraiva</Text>
-          </Center>
-        </Stack>
-      </Center>
-    </>
+              <Button onClick={openModal}>
+                Show me 
+              </Button>
+            </Group>
+          </Stack>
+        </Center>
+      </AppShell.Main>
+      <AppShell.Footer withBorder={false}>
+        <Group h={"100%"} justify="center">
+          <Text size="sm">2024 nfpsaraiva</Text>
+        </Group>
+      </AppShell.Footer>
+    </AppShell>
   );
 }
